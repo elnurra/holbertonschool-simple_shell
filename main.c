f0f5230998c9dfabd5505fe28d7fa72c374c5875
Elnurra <elnurra@code.edu.az>
2023-12-29 13:28:37 -0800

    feat: added some tasks

--- main.c
+++ main.c
@@ -1,36 +1,183 @@
-#include "shell.h"
-
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#define CHAR_BUFFER 1024
 /**
- * main - function that executes input we type and gives us the output
- * @ac: size of av
- * @av: array size of ac
- * Return: returns (0) in Success
+ * line_devider - devide the line
+ * @buffer: string
+ *
+ * Return: char ptr to ptr
  */
-
-int main(int ac, char **av)
+char **line_devider(char *buffer, char **arr)
 {
-	char *buffer = NULL, **cmmd = NULL;
-	int status = 0, i = 0;
-	(void)ac;
+	char *token;
+	int i = 0;
 
+	token = strtok(buffer, " \n\t");
+	while (token != NULL && i < 63)
+	{
+		arr[i++] = token;
+		token = strtok(NULL, " \n\t");
+	}
+	arr[i++] = NULL;
+	return (arr);
+}
+/**
+ * get_input - input part
+ * @len: len
+ * @buffer: buff
+ *
+ * Return: void
+ */
+char *get_input(void)
+{
+	char *buffer = NULL;
+        size_t len = 0;
+	int read;
+	if (isatty(STDIN_FILENO))
+		printf("$ ");
+	read = getline(&buffer, &len, stdin);
+	if (read == -1)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	if (buffer[read - 1] == '\n')
+                buffer[read - 1] = '\0';
+	return (buffer);
+}
+/**
+ * free_array - free array memory
+ * @arr: buffer
+ *
+ * Return - nothing
+ */
+void free_array(char **arr)
+{
+	int i;
+	if (arr == NULL)
+		return;
+	for (i = 0; arr[i] != NULL; i++)
+	{
+		free(arr[i]);
+	}
+	free(arr);
+	arr = NULL;
+}
+/**
+ * path_handler - path handler
+ * @file_name: name of file
+ *
+ * @path: path
+ *
+ * return: file_name
+ */
+char *path_handler(char *file_name, char *path)
+{
+	char *token = strtok(path, ":");
+	char cmd[100];
+	if (file_name[0] == '/')
+	{
+		if (access(file_name, X_OK) == 0)
+		{
+			return strdup(file_name);
+		}
+	}
+	while (token)
+	{
+		snprintf(cmd, sizeof(cmd), "%s/%s", token, file_name);
+		if (access(cmd, X_OK) == 0)
+		{
+			return strdup(cmd);
+		}
+		token = strtok(NULL, ":");
+	}
+	return strdup(file_name);
+}
+/**
+ * main - main func
+ *
+ * Return: int
+ */
+int main(void)
+{
+	extern char **environ;
+	char *buffer = NULL;
+	int status = 0;
+	pid_t pid;
+	char *path;
+	char *original_command;
 	while (1)
 	{
-		buffer = _getline();
+		buffer = get_input();
 		if (buffer == NULL)
+			break;
+		if (strcmp(buffer, "exit") == 0)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			return (status);
+			free(buffer);
+			exit(0);
 		}
-		i++;
-
-		cmmd = _split(buffer);
-		if (!cmmd)
-			continue;
-
-		if (_builtin(cmmd[0]))
-			builtins_handler(cmmd, av, &status, i);
+		pid = fork();
+		if (pid == 0)
+		{
+			char *arr[100];
+			line_devider(buffer, arr);
+			original_command = strdup(arr[0]);
+			path = getenv("PATH");
+			if (path == NULL || *path == '\0')
+			{
+				if (arr[0][0] == '/')
+				{
+					arr[0] = path_handler(arr[0], path);
+					if (execve(arr[0], arr, environ) == -1)
+					{
+						perror("ERROR");
+						free_array(arr);
+						free(buffer);
+						exit(1);
+					}
+				}
+				else
+				{
+					char error_message[CHAR_BUFFER];
+					snprintf(error_message, sizeof(error_message), "./hsh: 1: %s: not found\n",original_command);
+					write(STDERR_FILENO, error_message, strlen(error_message));
+					exit(127);
+				}
+			}
+			arr[0] = path_handler(arr[0], path);
+			if (execve(arr[0], arr, environ) == -1)
+			{
+				perror("ERROR");
+				exit(1);
+			}
+		}
+		else if (pid > 0)
+		{
+			if (waitpid(pid, &status, 0) == -1)
+			{
+				perror("ERROR");
+			}
+			if (WIFEXITED(status))
+			{
+				int exit_status = WEXITSTATUS(status);
+				if (exit_status == 127)
+				{
+					exit(127);
+				}
+			}
+		}
 		else
-			status = _exec(cmmd, av, i);
+		{
+			perror("ERROR");
+		}
+		if (pid == -1)
+			perror("ERROR");
 	}
+	free(buffer);
+	return (status);
 }
--- shell.h
+++ shell.h
@@ -1,44 +1,14 @@
-#ifndef SHELL_H_
-#define SHELL_H_
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-
-#define DELIM " \t\n"
+#include <unistd.h>
 extern char **environ;
 
-/* PROGRAM FUNCTIONS */
-char *_getline(void);
-char **_split(char *buffer);
-int _exec(char **cmd, char **av, int i_ndex);
-char *_getenv(char *env_var);
-char *get_path(char *cmd);
-
-/* BUILTINS */
-int _builtin(char *cmmd);
-void builtins_handler(char **cmmd, char **av, int *status, int indx);
-void e_xit(char **cmmd, char **av, int *status, int indx);
-void _write_env(char **cmmd, int *status);
-
-/* FUNCTION HELPER */
-void _free2dArr(char **arr);
-void error_handle(char *name, char *cmd, int indx);
-char *_itoa(int num);
-void str_rev(char *str, int len);
-int is_positive(char *str);
-int _atoi(char *str);
-
-/* STRING FUNCTIONS */
-int _strlen(const char *s);
-char *_strcpy(char *dest, const char *src);
-char *_strcat(char *dest, const char *src);
-int _strncmp(const char *s1, const char *s2, unsigned int n);
-char *_strdup(char *src);
-
-#endif /* _SHELL_H */
+int delim_counter(char *str, char *delim);
+char **token_parse(char *str, char *delim);
+char *check_file(char **path_arr, char *filepath, int *flag);
+int execute(char **args, char *filename, int *status);
+void _printenv(void);
+#endif
